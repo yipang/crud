ba898757cc375562584ef624319deccff6316a08
Yi Pang <pangyibobo@gmail.com>
2015-10-28 06:13:38 -0700

    another try

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # crud
 challenge 4
-<br />
+<br>
 I did not receive help from other sources except from teacher and TA
+<br>
+no
+<br>
+20
+<br>
+just start early will really help
 
--- js/help.js
+++ js/help.js
@@ -10,9 +10,9 @@ var total = 0.0;
 
 $('form').submit(function() {
 	var comment = new Review();
-	var numOfStars = $('#star').raty('score');
+	var numOfStars = Number($('#star').raty('score'));
 	comment.set('rate', numOfStars);
-	comment.set('title', $('#reviewTitle').val());
+	comment.set('title', $('#title').val());
 	commnet.set('review', $('#review').val());
 
 	comment.save(null, {
@@ -35,28 +35,62 @@ var getData = function() {
 }
 
 var buildList = function(data) {
-	$('ol').emtpy();
+	count = data.length;
+
+	$('#reviews').empty();
 	data.forEach(function(d) {
 		addItem(d)
+	});
+
+	$('#averageRating').raty({
+		score: total / count,
+		readOnly: true,
+		halfShow: true
 	});
 }
 
 var addItem = function(item) {
-	var title = item.get('reviewTitle');
+	var title = item.get('title');
 	var review = item.get('review');
 	var rate = item.get('rate');
-	count++;
 	total += rate;
-	average = Math.floor(total / count * 10) / 10;
-	var show = $('<p class=\"average\">Average Rating:<br>' + average + '</p>');
-	show.raty({score: average, readOnly: true, halfShow: true});
-	$('#header').append(show);
-	var info = $('<div><h3>' + title +'</h3></div><p>' + review + '</p></div>')
-	var up = $("<button class=\"btn btn-default btn-xs\"><i class=\"fa fa-thumbs-o-up\"></i></button>");
-	var down = $("<button class=\"btn btn-default btn-xs\"><i class=\"fa fa-thumbs-o-down\"></i></button>");
-	info.append(up);
-	info.append(down);
-	$('ol').append(info);
+
+	var space = $('<div></div>');
+	var showTitle = $('<h3>' + title + '</h3>');
+	var showReview = $('<p>' + review + '</p>');
+	var stars = $('<div id=rate>' + rate + '</div>');
+
+	var agree = $('<button class="btn btn-default btn-xs"><i class="fa fa-thumbs-o-up"></i></button>');
+	var disagree = $('<button class="btn btn-default btn-xs"><i class="fa fa-thumbs-o-down"></i></button>');
+	var remove = $('<button class="btn-xs btn-danger"><span class="glyphicon glyphicon-remove"></span></button>');
+
+	space.append(showTitle);
+	space.append(showReview);
+	space.append(agree);
+	space.append(disagree);
+	space.append(remove);
+	$('#reviews').append(space);
+	$('#rate' + rate).raty({
+		score:curScore,
+		readOnly: true
+	});
+
+	agree.click(function() {
+		item.save(null, {
+			success: getData
+		});
+	});
+	disagree.click(function() {
+		item.save(null, {
+			success: getData
+		});
+	});
+	remove.click(function() {
+		item.destroy({
+			success: getData
+		})
+	});
+
 }
 
 getData();
